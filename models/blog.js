c5cbea21e2558738d2479050fa8e8c9f185aa1e8
sjq1412 <sarahdev.cod3@gmail.com>
2023-08-27 22:33:17 +0800

    setup and use eslint

--- models/blog.js
+++ models/blog.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose')
 
 const blogSchema = new mongoose.Schema({
-  title: String, 
+  title: String,
   author: String,
   url: String,
   likes: Number
--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -1,8 +1,8 @@
-const listHelper = require("../utils/list_helper")
+const listHelper = require('../utils/list_helper')
 
 test('dummy returns 1', () => {
-    const blogs = []
+  const blogs = []
 
-    const result = listHelper.dummy(blogs)
-    expect(result).toBe(1)
+  const result = listHelper.dummy(blogs)
+  expect(result).toBe(1)
 })
