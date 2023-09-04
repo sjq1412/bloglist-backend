d74aa6f4d4c1f2b04b3d6baa5590c3976d52eed2
sjq1412 <sarahdev.cod3@gmail.com>
2023-09-04 22:19:37 +0800

    implement token-based authentication

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const loginRouter = require('./controllers/login')
 const usersRouter = require('./controllers/users')
 const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
@@ -25,6 +26,7 @@ app.use(express.json())
 
 app.use(middleware.requestLogger)
 
+app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
 
